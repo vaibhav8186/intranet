eca4b0c5e846125c1eb89d01a746cc3b91dd9c61
Shweta <shweta@joshsoftware.com>
2014-12-02 17:43:13 +0530

    Removed user's association with leave_details

--- app/models/concerns/leave_available.rb
+++ app/models/concerns/leave_available.rb
@@ -1,10 +1,6 @@
 module LeaveAvailable
   extend ActiveSupport::Concern
   
-  included do
-    has_many :leave_details
-  end  
-   
   def calculate_remaining_leave(total_leave) 
     ((total_leave * (12 - (self.private_profile.date_of_joining.month - 1)))/12).ceil 
   end
