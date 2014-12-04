55b5cd1e737622454b4fdae39b3ad308edbe63ea
Shweta <shweta@joshsoftware.com>
2014-12-04 13:16:28 +0530

    Method to assign leave added

--- app/models/concerns/leave_available.rb
+++ app/models/concerns/leave_available.rb
@@ -2,9 +2,18 @@ module LeaveAvailable
   extend ActiveSupport::Concern
   
   def assign_leave
+    date_of_joining = self.private_profile.date_of_joining
+    self.employee_detail.update_attribute(:available_leaves, calculate_leave(date_of_joining))
   end   
-  
+ 
+  def calculate_leave(date_of_joining)
+    leaves = (13 - date_of_joining.month) * PER_MONTH_LEAVE
+    leaves = leaves - 1 if date_of_joining.day > 15
+    leaves
+  end
+
   def set_leave_details_per_year
+    self.employee_detail.update_attribute(:available_leaves, PER_MONTH_LEAVE*12)
   end
   
   def eligible_for_leave?
