a7ca8dbdd0b371e094ffd23811c03a13c2eae328
Shweta <shweta@joshsoftware.com>
2015-01-20 21:52:59 +0530

    Skip autherisation for newsletter web-version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   before_filter :store_location
   before_filter :check_for_light
   skip_before_filter :verify_authenticity_token, only: :blog_publish_hook
-
+  
   def store_location
     unless INVALID_REDIRECTIONS.include?(request.fullpath) && request.xhr?
       if session[:previous_url].blank? || session[:previous_url] == '/'
@@ -24,6 +24,7 @@ class ApplicationController < ActionController::Base
   end
 
   def check_for_light
+    return if params[:controller].eql?('light/newsletters') and params[:action].eql?('web_version')
     if request.url.include?('newsletter') and !current_user.try(:role).in?(['Admin', 'HR', 'Super Admin'])
       flash[:error] = 'You are not authorized to access this page.'
       redirect_to main_app.root_url and return 
