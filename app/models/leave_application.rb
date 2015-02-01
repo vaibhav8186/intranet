e45937c44c96fca80187ef35eba6eba64cd9effb
Shweta <shweta@joshsoftware.com>
2015-02-01 15:43:32 +0530

    Added validation for leave
    - User should not be able to apply for leave multiple times for a date

--- app/models/leave_application.rb
+++ app/models/leave_application.rb
@@ -23,17 +23,18 @@ class LeaveApplication
   validate :validate_available_leaves, on: [:create, :update]
   validate :validate_leave_status, on: :update
   validate :end_date_less_than_start_date, if: 'start_at.present?'
+  validate :validate_date, on: [:create, :update]
 
   after_create :deduct_available_leave_send_mail
   after_update :update_available_leave_send_mail, if: "pending?"
 
   scope :pending, ->{where(leave_status: 'Pending')}
   scope :processed, ->{where(:leave_status.ne => 'Pending')}
- 
+
   def process_after_update(status)
     send("process_#{status}") 
   end
- 
+
   def pending?
     leave_status == 'Pending'
   end
@@ -60,35 +61,45 @@ class LeaveApplication
   end
 
   private
-    def deduct_available_leave_send_mail
-      user = self.user
-      user.employee_detail.deduct_available_leaves(number_of_days)    
-      user.sent_mail_for_approval(self.id) 
-    end
+  def deduct_available_leave_send_mail
+    user = self.user
+    user.employee_detail.deduct_available_leaves(number_of_days)    
+    user.sent_mail_for_approval(self.id) 
+  end
 
-    def update_available_leave_send_mail
-      user = self.user
-      prev_days, changed_days = number_of_days_change ? number_of_days_change : number_of_days
-      user.employee_detail.add_rejected_leave(prev_days)
-      user.employee_detail.deduct_available_leaves(changed_days||prev_days)
-      user.sent_mail_for_approval(self.id) 
-    end
+  def update_available_leave_send_mail
+    user = self.user
+    prev_days, changed_days = number_of_days_change ? number_of_days_change : number_of_days
+    user.employee_detail.add_rejected_leave(prev_days)
+    user.employee_detail.deduct_available_leaves(changed_days||prev_days)
+    user.sent_mail_for_approval(self.id) 
+  end
 
 
-    def validate_available_leaves
-      available_leaves = self.user.employee_detail.available_leaves
-      errors.add(:base, 'Not Sufficient Leave! Contact Administrator ') if available_leaves < number_of_days
-    end
+  def validate_available_leaves
+    available_leaves = self.user.employee_detail.available_leaves
+    errors.add(:base, 'Not Sufficient Leave! Contact Administrator ') if available_leaves < number_of_days
+  end
 
-    def validate_leave_status
-      if ["Rejected", "Approved"].include?(self.leave_status_was) && ["Rejected", "Approved"].include?(self.leave_status)
-        errors.add(:base, 'Leave is already processed')
-      end  
-    end
+  def validate_leave_status
+    if ["Rejected", "Approved"].include?(self.leave_status_was) && ["Rejected", "Approved"].include?(self.leave_status)
+      errors.add(:base, 'Leave is already processed')
+    end  
+  end
 
-    def end_date_less_than_start_date
-      if end_at < start_at
-        errors.add(:end_at, 'should not be less than start date.')
-      end
+  def end_date_less_than_start_date
+    if end_at < start_at
+      errors.add(:end_at, 'should not be less than start date.')
     end
+  end
+
+  def validate_date
+    leave_applications = self.user.leave_applications.ne(leave_status: LEAVE_STATUS[2])
+    leave_applications.each do |leave_application|
+      errors.add(:base, "Already applied for leave on same date") and return if self.start_at.between?(leave_application.start_at, leave_application.end_at) or
+        self.end_at.between?(leave_application.start_at, leave_application.end_at) or
+        leave_application.start_at.between?(self.start_at, self.end_at) or
+        leave_application.end_at.between?(self.start_at, self.end_at)
+    end
+  end
 end
