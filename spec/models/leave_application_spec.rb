b6049a6c3fdb493d0e35e26b504d9be5797f641b
Anuja Ware <anuja@joshsoftware.com>
2015-05-19 18:55:22 +0530

    Added specs and scenarios for leave's cross date validation
    To avoid leave for same date

--- spec/models/leave_application_spec.rb
+++ spec/models/leave_application_spec.rb
@@ -11,6 +11,35 @@ describe LeaveApplication do
     it { should validate_numericality_of(:contact_number) }
   end
 
+  context 'Validate date - Cross date validation - ' do
+
+    before do
+      @user = FactoryGirl.create(:user, role: 'Employee')
+      @user = FactoryGirl.create(:user, private_profile: FactoryGirl.build(:private_profile, date_of_joining: Date.new(Date.today.year, 01, 01))) 
+    end
+
+
+    it 'should not be able to apply leave for same date' do
+      date = Date.today
+      leave_application = FactoryGirl.create(:leave_application, user_id: @user.id, number_of_days: 2, start_at: date, end_at: (date + 1.day))
+      leave_application2 = FactoryGirl.build(:leave_application, user_id: @user.id, number_of_days: 1, start_at: (date + 1.day), end_at: (date + 1.day))
+      expect(leave_application2.valid?).to eq(false)
+      leave_application2.errors[:base].should eq(["Already applied for leave on same date"])
+    end
+
+    it 'start date should not exists in the range of applied leaves'
+    it 'end date should not exists in the range of applied leaves'
+    it 'no start date of any existing leave should come in the range of leave appling '
+    it 'no end date of any existing leave should come in '
+
+  end
+
+  context 'Leave can not be update ' do
+
+    it 'if already proccessed i.e Approved/Rejected'
+    
+  end
+
   context 'Method specs' do
 
     before do
@@ -24,7 +53,7 @@ describe LeaveApplication do
       expect(leave_application.valid?).to eq(true)
     end
 
-    it 'end date should be less than start date' do
+    it 'end date should not be less than start date' do
       date = Date.today
       leave_application = FactoryGirl.build(:leave_application, user_id: @user.id, number_of_days: 2, start_at: date, end_at: (date - 1.day))
       expect(leave_application.valid?).to eq(false)
