30df1327f75c96ffeea0d0274731f427e0c753d4
Anuja Ware <anuja@joshsoftware.com>
2015-05-27 15:14:26 +0530

    Success message on object saved successfully

--- app/controllers/concerns/restful_action.rb
+++ app/controllers/concerns/restful_action.rb
@@ -3,10 +3,10 @@ module RestfulAction
 
   def update_obj(model_obj, params, redirect_path)
     if model_obj.update_attributes(params)
-      flash[:notice] = "#{model_obj.class.to_s} updated Succesfully" 
+      flash[:success] = "#{model_obj.class.to_s} updated Succesfully" 
       redirect_to redirect_path
     else
-      flash[:alert] = "#{model_obj.class.to_s}: #{model_obj.errors.full_messages.join(',')}" 
+      flash[:error] = "#{model_obj.class.to_s}: #{model_obj.errors.full_messages.join(',')}" 
       render 'edit'
     end
   end
