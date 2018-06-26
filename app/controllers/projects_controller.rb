a647aab4d27fa999ff3d779b0368996237676b60
ajinath <ajinath@joshsoftware.com>
2018-06-26 16:19:33 +0530

    [#110] - Added feature to manage team member form project show screen.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,12 +1,12 @@
 class ProjectsController < ApplicationController
-    
-  load_and_authorize_resource 
+
+  load_and_authorize_resource
   skip_load_and_authorize_resource :only => :create
   before_action :authenticate_user!
-  before_action :load_project, except: [:index, :new, :create]
+  before_action :load_project, except: [:index, :new, :create, :remove_team_member, :add_team_member]
 
   include RestfulAction
-  
+
   def index
     @projects = Project.sort_by_position
     @projects.init_list!
@@ -23,14 +23,14 @@ class ProjectsController < ApplicationController
 
   def create
     @project = Project.new(safe_params)
-    if @project.save 
+    if @project.save
       flash[:success] = "Project created Succesfully"
       redirect_to projects_path
     else
       render 'new'
     end
   end
-  
+
   def update
     @project.user_ids = [] if params[:user_ids].blank? and params['tech_details'].nil?
     update_obj(@project, safe_params, projects_path)
@@ -39,10 +39,10 @@ class ProjectsController < ApplicationController
   def show
     @users = @project.users
   end
-  
+
   def destroy
     if @project.destroy
-     flash[:notice] = "Project deleted Succesfully" 
+     flash[:notice] = "Project deleted Succesfully"
     else
      flash[:notice] = "Error in deleting project"
     end
@@ -54,13 +54,26 @@ class ProjectsController < ApplicationController
     render nothing: true
   end
 
+  def remove_team_member
+    team_member = @project.users.find(params[:user_id])
+    @project.user_ids.delete(team_member.id)
+    @project.save
+    @users = @project.reload.users
+  end
+
+  def add_team_member
+    @project.user_ids = params[:project][:user_ids]
+    @project.save
+    @users = @project.reload.users
+  end
+
   private
   def safe_params
     params.require(:project).permit(:name, :start_date, :end_date, :managed_by, :code_climate_id, :code_climate_snippet,
     :code_climate_coverage_snippet, :is_active, :ruby_version, :rails_version, :database, :database_version, :deployment_server,
     :deployment_script, :web_server, :app_server, :payment_gateway, :image_store, :index_server, :background_jobs, :sms_gateway,
-    :other_frameworks,:other_details, :image, :url, :description, :case_study,:logo, :visible_on_website, :website_sequence_number, 
-    :code, :number_of_employees, :invoice_date, :company,
+    :other_frameworks,:other_details, :image, :url, :description, :case_study,:logo, :visible_on_website, :website_sequence_number,
+    :code, :number_of_employees, :invoice_date, :company_id,
     :user_ids => [])
   end
 
