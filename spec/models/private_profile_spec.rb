e35eb06e3646600adf1c17d05125eec3a84e2f3e
ajaysudhakaranarse <ajayanarse10@gmail.com>
2018-07-05 15:15:26 +0530

    Performed changes in test cases 1. Wrote test cases by considering role of user

--- spec/models/private_profile_spec.rb
+++ spec/models/private_profile_spec.rb
@@ -15,49 +15,73 @@ describe PrivateProfile do
   it { should validate_presence_of(:personal_email).on(:update) }
 =end
 
-  context 'Validate Date of joining' do
-    it 'should not update user beacuse joining date is not present' do
-      user = FactoryGirl.create(:user)
+  context 'Validate date of joining' do
+    let!(:user) { FactoryGirl.create(:user) }
+
+    before do
       user.status = 'approved'
       private_profile = user.private_profile
       private_profile.date_of_joining = ''
+    end
 
+    after do
       expect(user.save).to eq(false)
       expect(user.generate_errors_message).to eq("Private profile is invalid  Date of joining can't be blank")
     end
 
-    context 'validation' do
-      let!(:user){ FactoryGirl.create(:user) }
-
-      after do
-        expect(user.save).to eq(true)
-        expect(user.valid?).to eq(true)
-      end
-
-      it 'validation should not trigger because role is not employee' do
-        user.role = 'Intern'
-        byebug
-        expect(user.role).to_not be('Employee')
-      end
-
-      it 'validayion should not trigger because role is not HR' do
-        user.role = 'Intern'
-        expect(user.role).to_not be('HR')
-      end
+    it 'should not update user because joining date is not present, role is employee' do
+      user.role = 'Employee'
     end
 
-    it 'Validation should not trigger on create' do
-      user = FactoryGirl.create(:user)
+    it 'should not update user because joining date is not present, role is HR' do
+      user.role = 'HR'
+    end
+  end
 
+  context 'validation should not trigger' do
+    let!(:user){ FactoryGirl.create(:user) }
+
+    before do
+      user.role = 'Intern'
+      user.private_profile.date_of_joining = ''
+    end
+
+    after do
+      expect(user.save).to eq(true)
       expect(user.valid?).to eq(true)
     end
 
-    it 'should update user because joing date is present' do
-      user = FactoryGirl.create(:user)
+    it 'is not employee' do
+      expect(user.role).to_not be('Employee')
+    end
 
+    it 'is not HR' do
+      expect(user.role).to_not be('HR')
+    end
+  end
+
+  it 'Validation should not trigger on create' do
+    user = FactoryGirl.create(:user)
+
+    expect(user.valid?).to eq(true)
+  end
+
+  context 'Update user' do
+    let!(:user){ FactoryGirl.create(:user) }
+
+    after do
       expect(user.save).to eq(true)
       expect(user.generate_errors_message).to eq('  ')
     end
+
+    it 'should update user because joing date is present, role is employee' do
+      user.role = 'Employee'
+    end
+
+    it 'should update user because joing date is present, role is HR' do
+      user.role = 'HR'
+    end
   end
+
 end
   
