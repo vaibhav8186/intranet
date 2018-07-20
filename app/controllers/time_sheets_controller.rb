f64a14166663c3c5419736747f69aabae33dff70
ajaysudhakaranarse <ajayanarse10@gmail.com>
2018-07-20 19:43:05 +0530

    [#119] Fixed the scenario
    1. Date and time is not greater than current date and time.
    2. Check project display name with case insensitive.
    3. For same date and time record should not be create.
    4. Added data type for attributes in model
    5. Wrote test cases for same

--- app/controllers/time_sheets_controller.rb
+++ app/controllers/time_sheets_controller.rb
@@ -7,17 +7,22 @@ class TimeSheetsController < ApplicationController
     @time_sheet = TimeSheet.new
     ret, time_sheets_data = @time_sheet.parse_timesheet_data(params) unless params['user_id'].nil?
     if ret == true
-      create_time_sheet(time_sheets_data)
+      create_time_sheet(time_sheets_data, params)
     else
       render json: { text: '' }, status: :bad_request
     end
   end
 
-  def create_time_sheet(time_sheets_data)
+  def create_time_sheet(time_sheets_data, params)
     @time_sheet.attributes = time_sheets_data
     if @time_sheet.save
       render json: { text: "*Timesheet save successfully*" }, status: :created
     else
+      if @time_sheet.errors.full_messages.present?
+        text ="\`Error :: #{@time_sheet.errors.full_messages.join(', ')}\`"
+        @time_sheet.post_message_to_slack(params['channel_id'], text)
+      end
+
       render json: { text: 'fail' }, status: :unprocessable_entity
     end
   end
