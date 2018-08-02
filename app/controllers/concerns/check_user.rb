e68db73fdf71cc7c4fe92b5aea347a1686f527ad
ajaysudhakaranarse <ajayanarse10@gmail.com>
2018-08-02 22:43:40 +0530

    [#119] Fixed the feedback.

--- app/controllers/concerns/check_user.rb
+++ app/controllers/concerns/check_user.rb
@@ -1,8 +1,8 @@
 module CheckUser
   extend ActiveSupport::Concern
 
-  def is_user_present?(user, user_id)
-    return true unless user.nil?
+  def user_present?(user, user_id)
+    return true if user.present?
     email = SlackApiService.new.get_user_info(user_id)
     existing_user = User.where(email: email) if email
     return false if existing_user.blank?
