6201748952ceada0015e2b34ac7372a980fd1b52
ajaysudhakaranarse <ajayanarse10@gmail.com>
2018-08-03 11:05:15 +0530

    [#119] Done changes 1. Optimised the code. 2. Used updated user if he hit command first time

--- app/controllers/concerns/check_user.rb
+++ app/controllers/concerns/check_user.rb
@@ -1,12 +1,11 @@
 module CheckUser
   extend ActiveSupport::Concern
 
-  def user_present?(user, user_id)
-    return true if user.present?
+  def call_slack_api_service_and_fetch_email(user_id)
     email = SlackApiService.new.get_user_info(user_id)
     existing_user = User.where(email: email) if email
     return false if existing_user.blank?
-    existing_user.first.public_profile.update_attribute(:slack_handle, user_id)
-    return true
+    return_value = existing_user.first.public_profile.update_attribute(:slack_handle, user_id)
+    return_value ? existing_user.first : false
   end
 end
