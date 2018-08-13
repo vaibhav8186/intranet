758708c330d8f67066c08289f3bd55f98050f83c
ajaysudhakaranarse <ajayanarse10@gmail.com>
2018-08-13 15:23:24 +0530

    [#119] Solved the error no_text 1.Update the test cases

--- app/models/slack_bot.rb
+++ app/models/slack_bot.rb
@@ -2,9 +2,8 @@ class SlackBot < SlackRubyBot::Bot
   include Mongoid::Document
   include CheckUser
 
-  def show_projects(projects, channel)
-    text = "#{projects.each_with_index.map{|project, index| project.prepend("#{index+1}. ")}.join("\n")}"
-    SlackApiService.new.post_message_to_slack(channel, text)
+  def prepend_index(projects)
+    "#{projects.each_with_index.map{|project, index| project.prepend("#{index+1}. ")}.join("\n")}"
   end
 
   def fetch_email_and_associate_to_user(user_id)
--- spec/models/slack_bot_spec.rb
+++ spec/models/slack_bot_spec.rb
@@ -1,94 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe SlackBot, type: :model do
-  context 'show_projects' do
-    it 'it should response true' do
-      projects = ["tpn", "Dealsignal"]
+  context 'Slack bot' do
+    it 'it should prepend index to projects array' do
+      projects = ['tpn', 'Dealsignal']
+      return_value = SlackBot.new.prepend_index(projects)
+      expect(return_value).to eq("1. tpn\n2. Dealsignal")
+    end
+
+    it 'Should return true because user id is valid' do
       slack_params = {
         'token' => SLACK_API_TOKEN,
-        'channel' => CHANNEL_ID,
-        'text' => projects
+        'user' => USER_ID
       }
-
-      VCR.use_cassette 'success' do
-        response = Net::HTTP.post_form(URI("https://slack.com/api/chat.postMessage"),slack_params)
+      VCR.use_cassette 'success_user_info' do
+        response = Net::HTTP.post_form(URI("https://slack.com/api/users.info"), slack_params)
         resp = JSON.parse(response.body)
         expect(resp['ok']).to eq(true)
       end
     end
 
-    context 'failure' do
-      let!(:projects) { ["tpn", "Dealsignal"] }
-
-      it 'it should response false because auth token invalid' do
-        slack_params = {
-          'token' => 'abcd.123',
-          'channel' => CHANNEL_ID,
-          'text' => projects
-        }
-
-        VCR.use_cassette 'failure_reason_token' do
-          response = Net::HTTP.post_form(URI("https://slack.com/api/chat.postMessage"),slack_params)
-          resp = JSON.parse(response.body)
-          expect(resp['ok']).to eq(false)
-          expect(resp['error']).to eq('invalid_auth')
-        end
-      end
-
-      it 'it should response false because channel id invalid' do
-        slack_params = {
-          'token' => SLACK_API_TOKEN,
-          'channel' => 'abcd',
-          'text' => projects
-        }
-
-        VCR.use_cassette 'failure_reason_channel' do
-          response = Net::HTTP.post_form(URI("https://slack.com/api/chat.postMessage"),slack_params)
-          resp = JSON.parse(response.body)
-          expect(resp['ok']).to eq(false)
-          expect(resp['error']).to eq('channel_not_found')
-        end
-      end
-
-      it 'it should response false because text is empty' do
-        projects = []
-        slack_params = {
-          'token' => SLACK_API_TOKEN,
-          'channel' => CHANNEL_ID,
-          'text' => projects
-        }
-
-        VCR.use_cassette 'failure_reason_text' do
-          response = Net::HTTP.post_form(URI("https://slack.com/api/chat.postMessage"),slack_params)
-          resp = JSON.parse(response.body)
-          expect(resp['ok']).to eq(false)
-          expect(resp['error']).to eq('no_text')
-        end
-      end
-
-      it 'Should return true because user id is valid' do
-        slack_params = {
-          'token' => SLACK_API_TOKEN,
-          'user' => USER_ID
-        }
-        VCR.use_cassette 'success_user_info' do
-          response = Net::HTTP.post_form(URI("https://slack.com/api/users.info"), slack_params)
-          resp = JSON.parse(response.body)
-          expect(resp['ok']).to eq(true)
-        end
-      end
-
-      it 'Should return false because user id is invalid' do
-        slack_params = {
-          'token' => SLACK_API_TOKEN,
-          'user' => 'ABCD8F'
-        }
-        VCR.use_cassette 'failure_user_info' do
-          response = Net::HTTP.post_form(URI("https://slack.com/api/users.info"), slack_params)
-          resp = JSON.parse(response.body)
-          expect(resp['ok']).to eq(false)
-          expect(resp['error']).to eq('user_not_found')
-        end
+    it 'Should return false because user id is invalid' do
+      slack_params = {
+        'token' => SLACK_API_TOKEN,
+        'user' => 'ABCD8F'
+      }
+      VCR.use_cassette 'failure_user_info' do
+        response = Net::HTTP.post_form(URI("https://slack.com/api/users.info"), slack_params)
+        resp = JSON.parse(response.body)
+        expect(resp['ok']).to eq(false)
+        expect(resp['error']).to eq('user_not_found')
       end
     end
   end
