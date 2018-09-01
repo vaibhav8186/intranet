d32755e3de3bbabdb86d031e9723ea8d38fa1e9c
Shweta kale <shweta@joshsoftware.com>
2018-09-01 14:52:47 +0530

    job seeker mails to career mail id

--- app/mailers/website_mailer.rb
+++ app/mailers/website_mailer.rb
@@ -4,6 +4,10 @@ class WebsiteMailer < ActionMailer::Base
  
   def contact_us(website_contact)
     @website_contact = website_contact
-    mail(subject: 'New contact request from website!', to: 'info@joshsoftware.com')
+    if @website_contact.role.eql?(WebsiteContact::CONTACT_US_JOB_SEEKER)
+      mail(subject: 'New contact request from website!', to: 'careers@joshsoftware.com')
+    else
+      mail(subject: 'New contact request from website!', to: 'info@joshsoftware.com')
+    end
   end
 end
--- app/models/website_contact.rb
+++ app/models/website_contact.rb
@@ -13,6 +13,8 @@ class WebsiteContact
   validates_presence_of :name
   validates :email, format: { with: EMAIL_ADDRESS, message: 'Invalid email'}
 
+  CONTACT_US_JOB_SEEKER = 'Job Seeker'
+
   after_create do
     WebsiteMailer.delay.contact_us(self)
   end
