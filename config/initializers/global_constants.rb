a7672963cbfd4824134d4a47ed5241ac37ddcea2
Ajay Anarse <ajay@joshsoftware.com>
2018-10-24 13:06:49 +0530

    Added leave status in constants

--- config/initializers/global_constants.rb
+++ config/initializers/global_constants.rb
@@ -2,6 +2,7 @@ GENDER = ['Male', 'Female']
 ADDRESSES = ['Permanent Address', 'Temporary Address']
 BLOOD_GROUPS = ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-']
 STATUS = ['created', 'pending', 'approved']
+LEAVE_STATUS = ['Pending', 'Approved', 'Rejected']
 INVALID_REDIRECTIONS = ["/users/sign_in", "/users/sign_up", "/users/password"]
 TSHIRT_SIZE = ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL']
 
