297e1d192194664616e3cdf5858e02a133e74de5
Ajay Anarse <ajay@joshsoftware.com>
2018-10-25 10:44:33 +0530

    Added roles in constants.

--- config/initializers/global_constants.rb
+++ config/initializers/global_constants.rb
@@ -16,6 +16,6 @@ CONTACT_ROLE =  ["Accountant", "Technical", "Accountant and Technical"]
 
 SLACK_API_TOKEN = ENV['SLACK_API_TOKEN']
 
-ROLE = { employee: 'Employee', HR: 'HR', manager: 'manager', team_member: 'team member' }
+ROLE = { employee: 'Employee', HR: 'HR', manager: 'Manager', team_member: 'Team member', intern: 'Intern' }
 
 EMAIL_ADDRESS = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
