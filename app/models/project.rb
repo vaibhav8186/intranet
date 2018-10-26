532ae541424b6b5aa54fe65376a95fd7424fdaf8
Ajay Anarse <ajay@joshsoftware.com>
2018-10-26 13:12:32 +0530

    Get approved users

--- app/models/project.rb
+++ app/models/project.rb
@@ -261,6 +261,6 @@ class Project
         ]
       }
     ]).pluck(:user_id).uniq
-    User.in(id: user_ids).where(status: STATUS[2], "$or"=>[{role: ROLE[:employee]}, {role: ROLE[:intern]}])
+    User.in(id: user_ids).where(status: STATUS[2])
   end
 end
