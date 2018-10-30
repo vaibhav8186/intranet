2c49c2e089405e1a6ac3bde759e7f8acb14ad755
Ajay Anarse <ajay@joshsoftware.com>
2018-10-30 15:28:37 +0530

    [#187] Done changes
    1. Wrote test cases for model and controller

--- spec/models/time_sheet_spec.rb
+++ spec/models/time_sheet_spec.rb
@@ -685,6 +685,8 @@ RSpec.describe TimeSheet, type: :model do
       total_minutes, users_without_timesheet = TimeSheet.get_time_sheet_and_calculate_total_minutes(user, project, from_date, to_date)
       expect(total_minutes).to eq(0)
       expect(users_without_timesheet).to eq(["fname lname", "The pediatric network", 1])
+    end
+  end
 
   context 'Update timesheet' do
     let!(:user) { FactoryGirl.create(:user, email: 'abc@joshsoftware.com', role: 'Admin') }
@@ -728,6 +730,24 @@ RSpec.describe TimeSheet, type: :model do
       params = {"time_sheets_attributes"=>{"0"=>{"project_id"=>"", "date"=>"#{Date.today - 10}", "from_time"=>"#{DateTime.now + 3.minutes}", "to_time"=>"#{DateTime.now + 5.minutes}", "description"=>"", "id"=>"#{time_sheet.id}"}}, "id"=>user.id}
       return_value = TimeSheet.check_validation_while_updating_time_sheet(params)
       expect(return_value).to eq('Error :: Not allowed to fill timesheet for this date. If you want to fill the timesheet, meet your manager.')
+    end
+  end
+
+  context 'Export project report' do
+    let!(:user_one) { FactoryGirl.create(:user) }
+    let!(:user_two) { FactoryGirl.create(:user) }
+    let!(:project) { FactoryGirl.create(:project) }
+    
+    it 'Should give the project report' do
+      UserProject.create(user_id: user_one.id, project_id: project.id, start_date: Date.today - 20, end_date: nil)
+      UserProject.create(user_id: user_two.id, project_id: project.id, start_date: Date.today - 20, end_date: nil)
+      user_one.time_sheets.create!(project_id: project.id, date: Date.today - 2, from_time: "#{Date.today - 2} 10", to_time: "#{Date.today - 2} 11", description: 'Test api')
+      user_one.time_sheets.create!(project_id: project.id, date: Date.today - 2, from_time: "#{Date.today - 2} 12", to_time: "#{Date.today - 2} 13", description: 'call with client')
+      user_two.time_sheets.create!(project_id: project.id, date: Date.today - 2, from_time: "#{Date.today - 2} 10", to_time: "#{Date.today - 2} 11", description: 'test data')
+      from_date = Date.today - 20
+      to_date = Date.today
+      project_report = TimeSheet.create_project_report_in_csv(project, from_date, to_date)
+      expect(project_report).to eq("Employee name,Date(dd/mm/yyyy),No of hours,Details\nfname lname,28-10-2018,2,\"Test api\ncall with client\"\nfname lname,28-10-2018,1,test data\n")
     end
   end
 
