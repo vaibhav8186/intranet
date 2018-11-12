470e5284f750c4871d438695ee3cd11046c95927
Ajay Anarse <ajay@joshsoftware.com>
2018-11-12 16:40:52 +0530

    Done changes
    Show users without project in users report

--- app/models/time_sheet.rb
+++ app/models/time_sheet.rb
@@ -266,7 +266,7 @@ class TimeSheet
     return time_sheet_log, time_sheet_message
   end
 
-  def self.generete_employee_timesheet_report(timesheets, from_date, to_date)
+  def self.generete_employee_timesheet_report(timesheets, from_date, to_date, current_user)
     timesheet_reports = []
     timesheets.each do |timesheet|
       user = load_user_with_id(timesheet['_id'])
@@ -289,7 +289,8 @@ class TimeSheet
       users_timesheet_data['leaves'] = approved_leaves_count(user, from_date, to_date)
       timesheet_reports << users_timesheet_data
     end
-    sort_on_user_name_and_project_name(timesheet_reports)
+    users_without_timesheet = get_users_without_timesheet(from_date, to_date, current_user)
+    return sort_on_user_name_and_project_name(timesheet_reports), users_without_timesheet
   end
 
   def self.create_projects_report_in_json_format(projects_report, from_date, to_date)
@@ -312,8 +313,7 @@ class TimeSheet
     end
     projects_report_in_json.sort!{|previous_record, next_record| previous_record['project_name'] <=> next_record['project_name']}
     project_without_timesheet = get_project_without_timesheet(project_names, from_date, to_date) if project_names.present?
-    users_without_timesheet = get_users_without_timesheet(from_date, to_date)
-    return projects_report_in_json, project_without_timesheet, users_without_timesheet
+    return projects_report_in_json, project_without_timesheet
   end
 
   def self.generate_individual_timesheet_report(user, params)
@@ -643,7 +643,8 @@ class TimeSheet
     unfilled_timesheet_projects.sort{|previous_record, next_record| previous_record['project_name'] <=> next_record['project_name']}
   end
 
-  def self.get_users_without_timesheet(from_date, to_date)
+  def self.get_users_without_timesheet(from_date, to_date, current_user)
+    return if current_user.role == ROLE[:employee] || current_user.role == ROLE[:intern]
     user_ids = TimeSheet.where(date: {"$gte" => from_date, "$lte" => to_date}).distinct(:user_id)
     users = User.not_in(id: user_ids)
     users.where(status: STATUS[2], "$or" => [{role: ROLE[:employee]}, {role: ROLE[:intern]}]).order("public_profile.first_name" => :asc)
