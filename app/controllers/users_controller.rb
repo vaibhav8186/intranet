8d1cb0f2da7f13cfae4ee39c24417914f1b7dfbe
Vaibhav Thombare <vaibhav.thombare@joshsoftware.com>
2019-03-28 16:56:01 +0530

    only approved users can download

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
       @users = User.all
     end
     @users = @users.sort{|u1, u2| u1.name.to_s <=> u2.name.to_s}
+    @approved_user = User.employees.approved
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @users }
